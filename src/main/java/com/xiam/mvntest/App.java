c3c8b6f18e1d0f34bc04ba304880f6e9bf457d59
unknown <apegum@apegum.eu.qualcomm.com>
2013-04-24 15:52:13 +0100

    adding stuff #1

---
+++ src/main/java/com/xiam/mvntest/App.java
@@ -0,0 +1,13 @@
+package com.xiam.mvntest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!23e" );
+    }
+}
